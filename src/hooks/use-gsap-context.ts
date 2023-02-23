991b9c555b591df37bb56fc9971ef1375c6d0c79
fagarcia086 <fabian.garcia2589@gmail.com>
2023-02-24 01:44:15 +0400

    fix: optional auto revert context

--- src/hooks/use-gsap-context.ts
+++ src/hooks/use-gsap-context.ts
@@ -1,8 +1,16 @@
 import { gsap } from 'gsap'
 import { MutableRefObject, useMemo } from 'react'
+import { useIsomorphicLayoutEffect } from 'src/hooks/use-isomorphic-layout-effect'
 
-export function useGsapContext<T = any>(scope: MutableRefObject<T>) {
+export function useGsapContext<T = any>(
+  scope: MutableRefObject<T>,
+  autoRevert?: boolean
+) {
   const context = useMemo(() => gsap.context(() => {}, scope), [scope])
+
+  useIsomorphicLayoutEffect(() => {
+    if (autoRevert) return () => context.revert()
+  }, [])
 
   return context
 }
